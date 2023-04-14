05a21556399b7cc08bf2176346d9b827dfe0c6e1
wujiu <1050705928@qq.com>
2023-04-15 03:22:31 +0800

    自定义事件

---
+++ 09-自定义事件/components/ComponentStudent.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="student">
+    <h2>学生姓名：{{ name }}</h2>
+    <h2>学生性别：{{ sex }}</h2>
+    <button @click="sendStudentName">把学生名给App</button>
+    <button @click="unbind">解绑custom事件</button>
+    <button @click="death">销毁当前Student组件的实例(vc)</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'ComponentStudent',
+    data() {
+        return {
+            name: '张三',
+            sex: '男',
+            number: 0,
+        };
+    },
+    methods: {
+        sendStudentName() {
+            // 触发组件实例对象身上的自定义事件
+            // this.$emit('demo');
+            this.$emit('custom', this.name);
+        },
+        unbind() {
+            this.$off('custom');
+            // this.$off(['custom', 'demo']);
+            // this.$off()
+        },
+        death() {
+            this.$destroy();
+        },
+    },
+
+};
+</script>
+
+<style scoped>
+.student {
+    background-color: pink;
+    padding: 5px;
+    margin-top: 30px;
+}
+</style>
---
+++ src/components/ComponentSchool.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="school">
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+    <button @click="sendSchoolName">把学校名给App</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'ComponentSchool',
+    data() {
+        return {
+            name: 'nuc',
+            address: 'china',
+        };
+    },
+    props: {
+        getSchoolName: {
+            type: Function,
+        },
+    },
+    methods: {
+        sendSchoolName() {
+            this.getSchoolName(this.name);
+        },
+    },
+};
+</script>
+
+<style scoped>
+.school {
+    background-color: skyblue;
+    padding: 5px;
+}
+</style>
